75ac35997bbc7a867e4af7d8e5b40f7960d92117
nolejnik <nata.olejnik98@gmail.com>
2024-08-06 21:28:31 +0200

    switch between sections

--- app.js
+++ app.js
@@ -16,4 +16,26 @@ function pageTransitions() {
     }
 }
 
-pageTransitions();
+//sections active class
+allSections.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if(id) {
+        //remove selected from the other btns
+        secBtns.forEach((btn) => {
+            btn.classList.remove('active')
+        })
+        e.target.classList.add('active')
+
+        //hide other sections
+        sections.forEach((section) => {
+            section.classList.remove('active')
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add('active');
+
+    }
+});
+
+
+pageTransitions(); 
